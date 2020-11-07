9c5801cd9b45a6444341b3202d703e5a53de1f02
Елисей Ли <elisey.li.2002@mail.ru>
2020-11-07 16:19:59 +1000

    add moduls

---
+++ Structs.h
@@ -0,0 +1,16 @@
+const int NumberOfDigits = 34;
+
+using namespace std;
+
+struct TLong
+{
+	unsigned short dataInt[NumberOfDigits] = { 0 };
+	unsigned short dataFloat[NumberOfDigits] = { 0 };
+	bool sign = false;
+};
+
+enum class ETAR {
+	NUMBER,
+	OPERATOR,
+	ERROR,
+};
